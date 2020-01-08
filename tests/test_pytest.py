1e19ee9dfa330435f205e0eaeae22d44cdbfc125
Olga <tomasmor42@gmail.com>
2020-01-08 08:22:20 +0100

    Removing failing tests

--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -40,9 +40,3 @@ def test_create_task_in_the_past():
     
     task = service.create_task(date, TASK_TEXT)
     assert task is None
-
-def test_fail():
-    assert False
-
-def test_fail_again():
-    assert False
